95a537a4be22a4fe97510bac1093259d375c9cd0
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-02-06 13:11:57 +0530

    2 hour task

--- js/admindata.js
+++ js/admindata.js
@@ -1,6 +1,47 @@
-function checkEnterData(){
+window.onclick=function(){
     var email = document.getElementById('email').value;
     var password = document.getElementById('pass').value;
-    
+    var enter_email = document.getElementById('enter_email').value;
+    var enter_password = document.getElementById('enter_pass').value;
 
+    
+    if(email!=null)
+    {
+        if(password!=null)
+        {
+            if(email==enter_email)
+            {
+                if(password==enter_password)
+                {
+                    alert ("Login successfully");
+                    window.location = "Dashboard.html";
+                }
+                else
+                {
+                    alert("Wrong Password");
+                }
+            }
+            else
+            {
+                alert("Invalid username and password")
+            }
+        }
+        else
+        {
+            alert("please enter password");
+        }
+    
+    }
+    else
+    {
+        if(password==null)
+        {
+            alert("please enter details ");
+        }
+        else
+        {
+            alert("please enter email");
+        }
+        
+    }
 }
