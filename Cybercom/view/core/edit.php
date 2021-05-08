757574e38a435c590b6e46e6502676f86d81bd49
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-05-08 15:13:43 +0530

    Delete Cybercom directory

--- Cybercom/view/core/edit.php
+++
@@ -1,6 +0,0 @@
-<table width="100%" height="100">
-    <tr>
-        <td width="200px"> <?php echo $this->getTabHtml() ?> </td>
-        <td> <?php echo $this->getTabContent() ?> </td>
-    </tr>
-</table>
