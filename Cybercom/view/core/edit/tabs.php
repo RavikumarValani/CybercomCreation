706ec1d101998da801d161b26ba5aad662c7f6a2
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-05-08 15:17:55 +0530

    Delete Cybercom directory

--- Cybercom/view/core/edit/tabs.php
+++
@@ -1,4 +0,0 @@
-<?php $tabs = $this->getTabs(); ?>
-<?php foreach ($tabs as $key => $tab): ?>
-    <a class="btn btn-primary" onclick="mage.setUrl('<?php echo $this->getUrl()->getUrl(null,null,['tab' => $key]); ?>').load()"><?php echo $tab['label']; ?></a><br><br>
-<?php endforeach; ?>
